00fca1494130e731467b76e6c2601001392f343a
demogi4523 <demogi4523@yandex.ru>
2022-04-18 00:24:19 +0800

    Relationships and hyperlinked APIs tutorial

--- README.md
+++ README.md
@@ -6,6 +6,6 @@
 1. [Serialization](https://www.django-rest-framework.org/tutorial/1-serialization/)
 2. [Requests and responses](https://www.django-rest-framework.org/tutorial/2-requests-and-responses/)
 3. [Class based views](https://www.django-rest-framework.org/tutorial/3-class-based-views/)
-4. [Authentication and permissions](https://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/) (this commit)
-5. [Relationships and hyperlinked APIs](https://www.django-rest-framework.org/tutorial/5-relationships-and-hyperlinked-apis/)
+4. [Authentication and permissions](https://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/)
+5. [Relationships and hyperlinked APIs](https://www.django-rest-framework.org/tutorial/5-relationships-and-hyperlinked-apis/) (this commit)
 6. [Viewsets and routers](https://www.django-rest-framework.org/tutorial/6-viewsets-and-routers/)
--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -4,15 +4,19 @@ from rest_framework import serializers
 
 from .models import Snippet
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
-
-class SnippetSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    class Meta:
-        model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+        fields = ['url', 'id', 'username', 'snippets']
